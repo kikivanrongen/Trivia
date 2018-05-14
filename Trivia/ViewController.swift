a85ebc3ed4e927d5dad0cc1fe712c2734a989037
Kiki van Rongen <Kiki@wcw-stud-145-109-39-96.wireless.uva.nl>
2018-05-14 10:29:05 +0200

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Kiki van Rongen on 14-05-18.
+//  Copyright © 2018 Kiki van Rongen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
